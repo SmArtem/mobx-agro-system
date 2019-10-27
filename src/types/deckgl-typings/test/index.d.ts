863be8325fc183913ae4c227f5d1c6b25ddb6288
Artem Zabolotskiy <smartem95@gmail.com>
2019-10-27 19:39:54 +0700

    copy deck.gl types

---
+++ src/types/deckgl-typings/test/index.d.ts
@@ -0,0 +1,17 @@
+/// <reference path="../luma.gl/index.d.ts" />
+/// <reference path="../deck.gl__core/index.d.ts" />
+
+import {
+    COORDINATE_SYSTEM,
+    Deck,
+    Layer,
+    LineLayer,
+    OrbitView,
+    PolygonLayer,
+    TextLayer
+} from 'deck.gl';
+import {
+    CubeGeometry,
+    fp64,
+    Model
+} from 'luma.gl';
